9c5b2febf610777d28354b5eda3f9f24e0454e83
fulya çakıroğlu <81028725+Fulyacakiroglu2000@users.noreply.github.com>
2022-06-23 14:43:11 +0300

    Add files via upload

--- hm3_m.m
+++ hm3_m.m
@@ -1,17 +1,16 @@
 I = imread("8.tif");
-subplot(2,3,1),imshow(I),title("first picture")
+subplot(2,5,1),imshow(I),title("first picture")
 med = medfilt2(I);
-subplot(2,3,2),imshow(med),title("noisless picture")
+subplot(2,5,2),imshow(med),title("noisless picture")
 
 
 PQ=paddedsize(size(med));
 H1=notch('btw',PQ(1),PQ(2),12,50,15);
 H2=notch('btw',PQ(1),PQ(2),12,945,730);
 
-subplot(2,3,3),imshow(fftshift(H1.*H2));
+subplot(2,5,3),imshow(fftshift(H1.*H2));
 
 ForTrans=fft2(double(med),PQ(1),PQ(2));
-
 
 FS_car = ForTrans.*H1.*H2;
 
@@ -20,9 +19,48 @@ F_car=real(ifft2(FS_car));
 
 F_car=F_car(1:size(med,1), 1:size(med,2));
 
-subplot(2,3,4), imshow(F_car,[]),title('blurred image')
+subplot(2,5,4), imshow(F_car,[]),title('butterworth blurred image')
+
+
+
+%---------------------------------------
+
+H3=notch('ideal',PQ(1),PQ(2),12,50,15);
+H4=notch('ideal',PQ(1),PQ(2),12,945,730);
+
+subplot(2,5,5),imshow(fftshift(H3.*H4));
+
+ForTrans=fft2(double(med),PQ(1),PQ(2));
+
+FS_car2 = ForTrans.*H3.*H4;
+
+F_car2=real(ifft2(FS_car2)); 
+
+
+F_car2=F_car2(1:size(med,1), 1:size(med,2));
+
+subplot(2,5,6), imshow(F_car2,[]),title('ideal blurred image')
+
+%-----------------------------------------
+H5=notch('gaussian',PQ(1),PQ(2),12,50,15);
+H6=notch('gaussian',PQ(1),PQ(2),12,945,730);
+
+subplot(2,5,7),imshow(fftshift(H5.*H6));
+
+ForTrans=fft2(double(med),PQ(1),PQ(2));
+
+FS_car3 = ForTrans.*H5.*H6;
+
+F_car3=real(ifft2(FS_car3)); 
+
+
+F_car3=F_car3(1:size(med,1), 1:size(med,2));
+
+subplot(2,5,8), imshow(F_car3,[]),title('gaussian blurred image')
+%-----------------------------------------
+
 sh_im=imsharpen(F_car);
-subplot(2,3,5),imshow(sh_im,[]),title('sharped image')
+subplot(2,5,9),imshow(sh_im,[]),title('sharped image')
 
 Fc=fftshift(ForTrans);
 Fcf=fftshift(FS_car);
